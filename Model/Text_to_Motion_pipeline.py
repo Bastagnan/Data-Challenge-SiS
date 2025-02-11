baecfb873766e8f0f133efdbb14275a526626b1b
EtiNL <etienne.lanzeray@gmail.com>
2025-02-11 16:24:28 +0100

    modified:   Model/Text_to_Motion_pipeline.py

--- Model/Text_to_Motion_pipeline.py
+++ Model/Text_to_Motion_pipeline.py
@@ -163,7 +163,7 @@ def train(model,
             # Forward pass with CLIP encoder -> motion predictor
             pred_motion = model(texts)  # shape => (batch_size, motion_dim) in float32
 
-            distance_loss = loss_distance_between_points_torch(torch.reshape(gt_motion, motion.size(0), 100, 22, 3) ,torch.reshape(pred_motion, motion.size(0), 100, 22, 3), A)
+            distance_loss = loss_distance_between_points_torch(torch.reshape(gt_motion, (motion.size(0), 100, 22, 3)) ,torch.reshape(pred_motion, (motion.size(0), 100, 22, 3)), A)
 
             loss = criterion(pred_motion, gt_motion) + distance_loss
             optimizer.zero_grad()
